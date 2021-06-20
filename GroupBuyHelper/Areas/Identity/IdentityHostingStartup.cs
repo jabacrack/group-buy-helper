ba5b4e8e5c981c888ad15190c97db62faea00b27
jabacrack <jabacrack@yandex.ru>
2021-06-20 15:11:42 +0300

    add identity pages

---
+++ GroupBuyHelper/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GroupBuyHelper.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GroupBuyHelper.Areas.Identity.IdentityHostingStartup))]
+namespace GroupBuyHelper.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
